d10a63605b17e7ac8c81f1a8535f9c7d0de97951
StepanZazyan <stepanzazyan@yandex.ru>
2021-07-16 10:30:50 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {};
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void show() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error lies = new Error(false, 0, "none");
+        Error truth = new Error(true, 1, "done");
+        Error soso = new Error(true, 0, "what");
+        lies.show();
+        truth.show();
+        soso.show();
+    }
+}
