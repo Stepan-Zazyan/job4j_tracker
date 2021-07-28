4c0cfb5687ea10d1d876adef7801db9dee6e1bc6
StepanZazyan <stepanzazyan@yandex.ru>
2021-07-28 13:20:13 +0300

    2.7. Реализация класса StartUI. Вывод заявки по id [#500748]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -36,7 +36,7 @@ public class StartUI {
                     for (Item item : items) {
                         System.out.println(item);
                     }
-                    } else {
+                } else {
                     System.out.println("Хранилище еще не содержит заявок");
                     }
             }   else if (select == 2) {
@@ -48,9 +48,9 @@ public class StartUI {
                 Item item = new Item(name);
                 if (tracker.replace(id, item)) {
                     System.out.println("Заявка изменена успешно.");
-                    } else {
+                } else {
                     System.out.println("Ошибка замены заявки.");
-                    }
+                }
             }   else if (select == 3) {
                 System.out.println("=== Delete item ====");
                 System.out.print("Enter id: ");
@@ -60,7 +60,17 @@ public class StartUI {
                 } else {
                     System.out.println("Ошибка удаления заявки.");
                 }
-            } else if (select == 6) {
+            }   else if (select == 4) {
+                System.out.println("=== Find item by id ====");
+                System.out.println("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                Item item = tracker.findById(id);
+                if (item != null) {
+                    System.out.println(item);
+                } else {
+                    System.out.println("Заявка с введенным id: " + id + " не найдена.");
+                }
+            }   else if (select == 6) {
                 run = false;
             }
         }
