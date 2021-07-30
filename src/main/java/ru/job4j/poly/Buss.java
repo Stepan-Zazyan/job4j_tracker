d3c15c1aa84bb32caed97f33d6290eafc24c6752
StepanZazyan <stepanzazyan@yandex.ru>
2021-07-30 22:52:08 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Buss.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Buss implements Transport {
+
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int passengers) {
+
+    }
+
+    @Override
+    public int load(int count) {
+        return 0;
+    }
+}
