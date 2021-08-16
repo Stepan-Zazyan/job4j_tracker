a9304fe6bbb9021a981c65f90484710139dbad52
StepanZazyan <stepanzazyan@yandex.ru>
2021-08-16 09:03:02 +0300

    2.1. Тест на ValidateInput [#311887 #190127]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -11,7 +11,7 @@ public class ValidateInputTest {
     public void whenInvalidInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"one", "1"}
+                new String[]{"one", "1"}
         );
         ValidateInput input = new ValidateInput(out, in);
         int selected = input.askInt("Enter menu:");
@@ -22,7 +22,7 @@ public class ValidateInputTest {
     public void whenValidInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"2"}
+                new String[]{"2"}
         );
         ValidateInput input = new ValidateInput(out, in);
         int selected = input.askInt("Enter menu:");
@@ -32,26 +32,24 @@ public class ValidateInputTest {
     @Test
     public void whenManyValidInput() {
         Output out = new StubOutput();
-        String[] array = new String[] {"1", "2", "3"};
+        String[] array = new String[]{"1", "2", "3"};
         Input in = new StubInput(array);
         ValidateInput input = new ValidateInput(out, in);
-        int selected =  0;
-        for (int i = 0; i < array.length; i++) {
-            selected = input.askInt("Enter menu:");
-        }
+        int selected = input.askInt("Enter menu:");
+        selected = input.askInt("Enter menu:");
+        selected = input.askInt("Enter menu:");
         assertThat(selected, is(3));
     }
 
     @Test
     public void whenMultipleNegativeValidInput() {
         Output out = new StubOutput();
-        String[] array = new String[] {"1", "2", "-3"};
+        String[] array = new String[]{"1", "2", "-3"};
         Input in = new StubInput(array);
         ValidateInput input = new ValidateInput(out, in);
-        int selected =  0;
-        for (int i = 0; i < array.length; i++) {
-            selected = input.askInt("Enter menu:");
-        }
+        int selected = input.askInt("Enter menu:");
+        selected = input.askInt("Enter menu:");
+        selected = input.askInt("Enter menu:");
         assertThat(selected, is(-3));
     }
 }
