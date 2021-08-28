47ec5ac598d3c7d1467b99bf791fbba57140b8bf
StepanZazyan <stepanzazyan@yandex.ru>
2021-08-28 11:38:08 +0300

    1. Паспорт и жители [#212656]

---
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -0,0 +1,29 @@
+package ru.job4j.collection;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PassportOffice {
+    private Map<String, Citizen> citizens = new HashMap<>();
+
+    public boolean add(Citizen citizen) {
+        boolean rsl = true;
+        if (citizens.containsKey(citizen.getPassport())) {
+            rsl = false;
+        } else {
+            citizens.put(citizen.getPassport(), citizen);
+        }
+        return rsl;
+    }
+    /*Сервис должен проверять, что гражданина с таким паспортом еще нет, а так же возможность получить информацию о гражданине по его паспорту.
+
+    Метод add вернет true, если пользователя еще нет в HashMap. Если он уже есть, то вернуть false.
+
+    Для решения этой задачи Вам потребуются методы Map.containsKey(key) и Map.get(key).*/
+
+
+    public Citizen get(String passport) {
+        return citizens.get(passport);
+    }
+}
