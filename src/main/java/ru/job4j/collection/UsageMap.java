3e1232e471e91a42d938a8c434360dd7b2a822fe
StepanZazyan <stepanzazyan@yandex.ru>
2021-08-28 10:57:12 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> testcoll = new HashMap<>();
+        testcoll.put("stepanzazyan@yandex.ru", "Stepan Sem");
+        testcoll.put("stepanzazyan@gmail.com", "Victor Step");
+        for (Map.Entry<String, String> key: testcoll.entrySet()) {
+            System.out.println(key.getKey() + " " + key.getValue());
+        }
+    }
+}
