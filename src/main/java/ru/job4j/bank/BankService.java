e89959e8308af703324f5a0128b0652c75b71763
Stepan-Zazyan <stepanzazyan@yandex.ru>
2022-06-25 22:02:42 +0300

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -2,13 +2,25 @@ package ru.job4j.bank;
 
 import java.util.*;
 
+/**
+ *модель для банковской системы
+ */
 public class BankService {
     private final Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     *Этот метод должен добавить пользователя в систему
+     * @param user
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<Account>());
     }
 
+    /**
+     *метод должен добавить новый счет к пользователю
+     * @param passport
+     * @param account
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -19,6 +31,11 @@ public class BankService {
         }
     }
 
+    /**
+     *метод ищет пользователя по номеру паспорта:
+     * @param passport
+     * @return
+     */
     public User findByPassport(String passport) {
         for (User x : users.keySet()) {
             if (x.getPassport().equals(passport)) {
@@ -28,6 +45,12 @@ public class BankService {
         return null;
     }
 
+    /**
+     *метод ищет счет пользователя по реквизитам
+     * @param passport
+     * @param requisite
+     * @return
+     */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -40,6 +63,15 @@ public class BankService {
         return null;
     }
 
+    /**
+     *метод предназначен для перечисления денег с одного счёта на другой счёт
+     * @param srcPassport
+     * @param srcRequisite
+     * @param destPassport
+     * @param destRequisite
+     * @param amount
+     * @return
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
