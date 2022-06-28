a64b1e1c6a799cd5d4825d849d4abcec6265751a
Stepan-Zazyan <stepanzazyan@yandex.ru>
2022-06-28 22:56:10 +0300

    1.1. Анонимные классы [#214142]

---
+++ src/main/java/ru/job4j/lambda/AttachmentSort.java
@@ -0,0 +1,26 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class AttachmentSort {
+    public static void main(String[] args) {
+        List<Attachment> attachments = Arrays.asList(
+                new Attachment("image 1", 100),
+                new Attachment("image 2", 34),
+                new Attachment("image 3", 13)
+        );
+        Comparator comparator = new Comparator() {
+            @Override
+            public int compare(Object o, Object t1) {
+                Attachment left = (Attachment) o;
+                Attachment right = (Attachment) t1;
+                return Integer.compare(left.getSize(), right.getSize());
+            }
+        };
+        attachments.sort(comparator);
+        System.out.println(attachments);
+        // Здесь создайте компаратор на основании анонимного класса.
+    }
+}
