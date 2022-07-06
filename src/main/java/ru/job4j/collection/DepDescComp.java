5a1cee7e0562e50b35bd704739f0713093461cbb
Stepan-Zazyan <stepanzazyan@yandex.ru>
2022-07-06 12:45:10 +0300

    2.2. Функциональный интерфейс [#247497]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,16 +8,17 @@ import java.util.List;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
+        int rsl = 2;
         List<String> oo1 = new ArrayList<>(Arrays.asList(o1.split("/")));
         List<String> oo2 = new ArrayList<>(Arrays.asList(o2.split("/")));
         for (int i = 0; i < Math.min(oo1.size(), oo2.size()); i++) {
-            if (oo1.get(i).compareTo(oo2.get(i)) != 0) {
-                return o2.compareTo(o1);
-            } else {
+            rsl = oo2.get(i).compareTo(oo1.get(i));
+            if (rsl == 0) {
                 return o1.compareTo(o2);
+            } else {
+                return rsl;
             }
         }
         return Integer.compare(o1.length(), o2.length());
-
     }
 }
---
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -0,0 +1,38 @@
+package ru.job4j.function;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.*;
+
+public class FunctionalInterfaces {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        BiConsumer<Integer, String> biCon = (t, u) -> map.put(t, u);
+        biCon.accept(1, "one");
+        biCon.accept(2, "two");
+        biCon.accept(3, "three");
+        biCon.accept(4, "four");
+        biCon.accept(5, "five");
+        biCon.accept(6, "six");
+        biCon.accept(7, "seven");
+        System.out.println(map);
+
+        BiPredicate<Integer, String> biPred = (t, u) -> (u.length() == 4 || t % 2 == 0);
+        for (Integer i : map.keySet()) {
+            System.out.print(biPred.test(i, map.get(i)) + " ");
+        }
+
+        Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
+
+        Consumer<String> con = (s) -> System.out.println(s);
+        Function<String, String> func = (t) -> t.toUpperCase();
+        for (String s : sup.get()) {
+            con.accept(func.apply(s));
+               /* Заменить вывод строк на применение Consumer
+                Заменить преобразование строк к строкам в верхнем регистре с помощью Function
+                Необходимое объявлено выше, требуется их реализовать.*/
+        }
+    }
+}
