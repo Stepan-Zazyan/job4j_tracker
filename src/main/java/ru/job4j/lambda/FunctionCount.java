f969e9f29cd87d74ce5f72c657d1c29d59379f34
Stepan-Zazyan <stepanzazyan@yandex.ru>
2022-07-14 13:20:53 +0300

    5. Подсчет функции в диапазоне. [#24251]

--- src/main/java/ru/job4j/lambda/FunctionCount.java
+++ src/main/java/ru/job4j/lambda/FunctionCount.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
-public class functionCount {
+public class FunctionCount {
     static List<Double> diapason(int start, int end, Function<Double, Double> func) {
         List<Double> function = new ArrayList<>();
-for (int i=start; i<end; i++) {
-    function.add(func.apply((double) i));
-}
+        for (int i = start; i < end; i++) {
+            function.add(func.apply((double) i));
+        }
         return function;
     }
 }
--- src/test/java/ru/job4j/lambda/FunctionCountTest.java
+++ src/test/java/ru/job4j/lambda/FunctionCountTest.java
@@ -1,26 +1,31 @@
 package ru.job4j.lambda;
+
 import org.junit.Test;
+
 import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
-public class functionCountTest{
+
+public class FunctionCountTest {
     @Test
     public void whenLinearFunctionThenLinearResults() {
-        List<Double> result = functionCount.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> result = FunctionCount.diapason(5, 8, x -> 2 * x + 1);
         List<Double> expected = Arrays.asList(11D, 13D, 15D);
         assertThat(result, is(expected));
     }
+
     @Test
     public void whenMultipleFunctionThenMultipleResults() {
-        List<Double> result = functionCount.diapason(2, 5, x -> Math.pow(x,2) + 1);
+        List<Double> result = FunctionCount.diapason(2, 5, x -> Math.pow(x, 2) + 1);
         List<Double> expected = Arrays.asList(5D, 10D, 17D);
         assertThat(result, is(expected));
     }
+
     @Test
     public void whenExponentialFunctionThenExponentialResults() {
-        List<Double> result = functionCount.diapason(2, 5, x -> Math.pow(2,x) + 1);
+        List<Double> result = FunctionCount.diapason(2, 5, x -> Math.pow(2, x) + 1);
         List<Double> expected = Arrays.asList(5D, 9D, 17D);
         assertThat(result, is(expected));
     }
