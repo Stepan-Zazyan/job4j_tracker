0277383ffdf776c14640a765cb27fcc6f68a401b
Stepan-Zazyan <stepanzazyan@yandex.ru>
2022-07-17 22:27:52 +0300

    1. Фильтрация учеников [#110058]

--- src/main/java/ru/job4j/stream/ProductLabel.java
+++ src/main/java/ru/job4j/stream/ProductLabel.java
@@ -7,14 +7,12 @@ import java.util.stream.Stream;
 public class ProductLabel {
     public List<String> generateLabels(List<Product> products) {
         return products.stream()
-                .filter(x-> x.getStandard()-x.getActual() <= 3)
-                .filter(x-> x.getStandard()-x.getActual() >= 0)
-                .map(x-> "Label{"
+                .filter(x -> x.getStandard() - x.getActual() <= 3)
+                .filter(x -> x.getStandard() - x.getActual() >= 0)
+                .map(x -> "Label{"
                         + "name='" + x.getName() + '\''
-                        + ", price=" + x.getPrice()*0.5
+                        + ", price=" + x.getPrice() * 0.5
                         + '}')
                 .collect(Collectors.toList());
-
-
     }
 }
--- src/main/java/ru/job4j/stream/StreamUsage.java
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -16,9 +16,15 @@ public class StreamUsage {
 
         @Override
         public String toString() {
-            return "Task{" +
-                    "name='" + name + '\'' +
-                    ", spent=" + spent +
+            return "Task{"
+                    +
+                    "name='"
+                    + name
+                    + '\''
+                    +
+                    ", spent="
+                    + spent
+                    +
                     '}';
         }
     }
