ffe150547c8e902356d410c87fd4811d10ef320b
Stepan-Zazyan <stepanzazyan@yandex.ru>
2022-07-22 11:41:46 +0300

    7. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#24260]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -3,21 +3,23 @@ package ru.job4j.bank;
 import java.util.*;
 
 /**
- *модель для банковской системы
+ * модель для банковской системы
  */
 public class BankService {
     private final Map<User, List<Account>> users = new HashMap<>();
 
     /**
-     *Этот метод должен добавить пользователя в систему
+     * Этот метод должен добавить пользователя в систему
+     *
      * @param user Принимает параметр user
      */
     public void addUser(User user) {
-        users.putIfAbsent(user, new ArrayList<Account>());
+        users.putIfAbsent(user, new ArrayList<>());
     }
 
     /**
-     *метод должен добавить новый счет к пользователю
+     * метод должен добавить новый счет к пользователю
+     *
      * @param passport Принимает String параметр паспорт
      * @param account  Принимает String параметр аккаунт
      */
@@ -32,44 +34,54 @@ public class BankService {
     }
 
     /**
-     *метод ищет пользователя по номеру паспорта:
+     * метод ищет пользователя по номеру паспорта:
+     *
      * @param passport Принимает String параметр паспорт
      * @return Возвращает объект user, или null в случае его отсутствия
      */
     public User findByPassport(String passport) {
-        for (User x : users.keySet()) {
+
+        return users.keySet()
+                .stream()
+                .filter(s -> s.getPassport().equals(passport))
+                .findFirst()
+                .orElse(null);
+        /*for (User x : users.keySet()) {
             if (x.getPassport().equals(passport)) {
                 return x;
             }
         }
-        return null;
+        return null;*/
     }
 
     /**
-     *метод ищет счет пользователя по реквизитам
-     * @param passport Принимает String параметр паспорт
+     * метод ищет счет пользователя по реквизитам
+     *
+     * @param passport  Принимает String параметр паспорт
      * @param requisite Принимает String параметр реквизиты
      * @return Возвращает реквизиты пользователя или null, если пользователя не существует
      */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user != null) {
-            for (Account x : users.get(user)) {
-                if (Objects.equals(x.getRequisite(), requisite)) {
-                    return x;
-                }
-            }
+            return users.values()
+                    .stream()
+                    .flatMap(Collection::stream)
+                    .filter(s -> s.getRequisite().equals(requisite))
+                    .findFirst()
+                    .orElse(null);
         }
         return null;
     }
 
     /**
-     *метод предназначен для перечисления денег с одного счёта на другой счёт
-     * @param srcPassport Принимает String параметр паспорт отправителя
-     * @param srcRequisite Принимает String параметр реквизиты отправителя
-     * @param destPassport Принимает String параметр паспорт получателя
+     * метод предназначен для перечисления денег с одного счёта на другой счёт
+     *
+     * @param srcPassport   Принимает String параметр паспорт отправителя
+     * @param srcRequisite  Принимает String параметр реквизиты отправителя
+     * @param destPassport  Принимает String параметр паспорт получателя
      * @param destRequisite Принимает String параметр реквизиты получателя
-     * @param amount Принимет double сумму перевода
+     * @param amount        Принимет double сумму перевода
      * @return Возвращает значение true в случае успешного перевода
      * и false, если перевод не был сделан
      */
