d9415c3d5488b6c29cac3f96aabc24ce67d206f4
Stepan-Zazyan <stepanzazyan@yandex.ru>
2022-07-22 12:54:28 +0300

    8. Шаблоны проектирования в Stream API   [#310012]

--- src/main/java/ru/job4j/collection/TrainingClass.java
+++ src/main/java/ru/job4j/collection/TrainingClass.java
@@ -15,7 +15,6 @@ public class TrainingClass {
                 map(x -> x * 2).
                 forEach(System.out::print); // 2, 4, 6
 
-        /*flatMap позволяет заменить элемент на Stream:*/
         List<Integer> integers2 = Arrays.asList(1, 2, 3);
         System.out.println();
         System.out.println("flatMap:");
