f9e3f836d8e698433473d2f7d43c7a54e2a12a5f
Stepan-Zazyan <stepanzazyan@yandex.ru>
2023-09-12 10:52:39 +0300

    2. @Setter, @Getter и другие [#504889 #444163]

--- src/main/java/ru/job4j/lombok/Category.java
+++ src/main/java/ru/job4j/lombok/Category.java
@@ -2,14 +2,12 @@ package ru.job4j.lombok;
 
 import lombok.*;
 
+@Getter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @RequiredArgsConstructor
 public class Category {
-    @NonNull
-    @Getter
     @EqualsAndHashCode.Include
     private int id;
-    @Getter
     @Setter
     private String name;
 
